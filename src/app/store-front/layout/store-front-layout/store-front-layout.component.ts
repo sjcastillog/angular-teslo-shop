e8e0f703dd524a1ae5cf2e1baf8e86081d66e841
Joao Castillo <joaocastillo91@gmail.com>
2025-09-19 15:15:48 -0500

    klsflks

---
+++ src/app/store-front/layout/store-front-layout/store-front-layout.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-store-front-layout',
+  imports: [RouterOutlet],
+  templateUrl: './store-front-layout.component.html',
+})
+export class StoreFrontLayoutComponent { }
